f2c800021d495da96783f743ad448b8b6b4426df
Débora <154550522+deboraayane@users.noreply.github.com>
2025-11-13 11:22:02 -0300

    Atlz

--- Assets/ObjCollect.cs
+++ Assets/ObjCollect.cs
@@ -1,16 +1,80 @@
+using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Rendering.CoreEditorDrawer<TData>;
 
-public class ObjCollect : MonoBehaviour
+uusing UnityEngine;
+using System.Collections.Generic;
+using System;
+
+
+public class CollectibleSpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    public GameObject collectiblePrefab;
+
+    
+    private List<Transform> spawnPoints;
+    public float timeBetweenSpawns = 5f;
+    private float spawnTimer;
+    public int maxCollectibles = 3;
+
     void Start()
     {
-        
+        spawnPoints = new List<Transform>();
+        foreach (Transform child in transform)
+        {
+            spawnPoints.Add(child);
+        }
+        if (spawnPoints.Count == 0)
+        {
+            Debug.LogError("Nenhum ponto de spawn encontrado!");
+            enabled = false;
+        }
+        spawnTimer = timeBetweenSpawns;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0)
+        {
+            TrySpawnCollectible();
+            spawnTimer = timeBetweenSpawns;
+        }
+    }
+
+    void TrySpawnCollectible()
+    {
+        if (GameObject.FindGameObjectsWithTag("Collectible").Length >= maxCollectibles) return;
+
+        int randomIndex = UnityEngine.Random.Range(0, spawnPoints.Count);
+        Transform chosenPoint = spawnPoints[randomIndex];
+
+       
+        if (Physics.OverlapSphere(chosenPoint.position, 0.5f).Length > 0)
+        {
+            
+            return;
+        }
+
+        Instantiate(collectiblePrefab, chosenPoint.position, Quaternion.identity);
+    }
+}
+
+
+public class Collectible : MonoBehaviour
+{
+    
+    private void OnTriggerEnter(Collider other)
+    {
         
+        if (other.CompareTag("Player"))
+        {
+           
+            Debug.Log("Objeto Coletado! Adicionar pontos aqui.");
+
+            
+            Destroy(gameObject);
+        }
     }
 }
